a06317b7996458f361f20fa12b80adcdfc08a099
ssabrut <michael.gunawan2002@gmail.com>
2023-06-30 11:31:39 +0700

    Add project structures

---
+++ app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name}
+    return {"model_name": model_name}
